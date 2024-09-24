7e8c4bd158a139dfb71a1f000632814236e94031
Mubashar Sadiq <131347972+MubasharSadiq@users.noreply.github.com>
2024-09-24 20:39:09 +0200

    Update deploy-armTemplate.sh

--- webServer-dev/deploy-armTemplate.sh
+++ webServer-dev/deploy-armTemplate.sh
@@ -5,6 +5,6 @@ az group create --name armRG --location northeurope
 az deployment group create \
 --resource-group armRG \
 --template-file armTemplate-webserver.json \
---parameters adminUsername=mobi \
+--parameters adminUsername=azureuser \
              adminPublicKey="$(cat ~/.ssh/id_rsa.pub)" \
              CustomData=@cloudinit-webserver.sh
